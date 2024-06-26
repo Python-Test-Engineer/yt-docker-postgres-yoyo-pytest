72042231af6f2d68ce29b5871abb0777f043498c
Craig West <iwswordpress@gmail.com>
2024-06-26 14:31:22 +0100

    restart with products

--- migrations/20240626_03_hK8wD-create-categories-table.py
+++ migrations/20240626_03_hK8wD-create-categories-table.py
@@ -4,8 +4,9 @@ Create categories table
 
 from yoyo import step
 
-__depends__ = {'20240626_02_WOcVI-add-products'}
+__depends__ = {"20240626_02_WOcVI-add-products"}
 
-steps = [
-    step("")
-]
+step(
+    "CREATE TABLE categories (id SERIAL PRIMARY KEY, cat_code varchar(10), cat_name varchar(50), is_active boolean DEFAULT true);",
+    "DROP TABLE categories;",
+),
