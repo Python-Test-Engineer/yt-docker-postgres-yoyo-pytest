f65229f6b7d638f1e4ec39c7fae13823bbc3f688
Craig West <iwswordpress@gmail.com>
2024-06-26 14:49:45 +0100

    add rollback_all

---
+++ rollback_all.py
@@ -0,0 +1,10 @@
+# scripts/rollback.py
+
+from yoyo import read_migrations, get_backend
+
+backend = get_backend(
+    "postgresql://postgres:postgres@host.docker.internal/postgres?port=5432"
+)
+migrations = read_migrations("./migrations")
+sorted_migrations = sorted(migrations, key=lambda x: x.id, reverse=True)
+backend.rollback_migrations(sorted_migrations)
